b65db321f87ec823ade29147369796644e705c37
Sanlim <5910110581@psu.ac.th>
2018-09-22 00:26:33 +0700

    7.11

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import "./App.css";
 import WordCard from './WordCard';
 
-const word = "Enginering";
+const word = "Engineering";
 class App extends Component {
   render() {
     return (
